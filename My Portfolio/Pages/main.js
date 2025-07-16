c5d9eb450f70f297bfcd3eace8deb29ceef3c22c
GoofyGoose1 <edridaniel2002@gmail.com>
2025-07-16 17:54:14 +0300

    Add files via upload

---
+++ My Portfolio/Pages/main.js
@@ -0,0 +1,24 @@
+function init() {
+    const main = document.getElementById('main-content');
+    main.classList.add('fade-in');
+
+    const links = document.querySelectorAll('a[href]');
+
+    links.forEach(link => {
+    link.addEventListener('click', function (e) {
+        const href = this.getAttribute('href');
+        if (!href || href.startsWith('#') || href.startsWith('mailto:')){
+            return;
+        }
+        e.preventDefault();
+        main.classList.remove('fade-in');
+        main.classList.add('fade-out');
+
+        setTimeout(() => {
+            window.location.href = href;
+        }, 600); 
+    });
+});
+}
+
+document.addEventListener('DOMContentLoaded', init);
